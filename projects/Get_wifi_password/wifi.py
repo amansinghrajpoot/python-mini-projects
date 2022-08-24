edad9bbb493b1febbe02be53f640c18fceb7fc54
Aman Singh Rajpoot <45413940+amansinghrajpoot@users.noreply.github.com>
2022-08-24 23:05:21 +0530

    Handled subprocess.CalledProcessError | There are some wi-fi names for which process couldn't fetch password.
    Traceback (most recent call last):
      File "C:\Users\Aman_Rajput\PycharmProjects\snipps\others\wifi-old.py", line 11, in <module>
        subprocess
      File "C:\Users\Aman_Rajput\AppData\Local\Programs\Python\Python39\lib\subprocess.py", line 424, in check_output
        return run(*popenargs, stdout=PIPE, timeout=timeout, check=True,
      File "C:\Users\Aman_Rajput\AppData\Local\Programs\Python\Python39\lib\subprocess.py", line 528, in run
        raise CalledProcessError(retcode, process.args,
    subprocess.CalledProcessError: Command '['netsh', 'wlan', 'show', 'profile', "Aman's Iphone", 'key=clear']' returned non-zero exit status 1.

--- projects/Get_wifi_password/wifi.py
+++ projects/Get_wifi_password/wifi.py
@@ -7,12 +7,15 @@ data = (
 )
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 for i in profiles:
-    results = (
-        subprocess
-        .check_output(["netsh", "wlan", "show", "profile", i, "key=clear"])
-        .decode("utf-8")
-        .split("\n")
-    )
+    try:
+        results = (
+            subprocess
+            .check_output(["netsh", "wlan", "show", "profile", i, "key=clear"])
+            .decode("utf-8")
+            .split("\n")
+        )
+    except subprocess.CalledProcessError:
+        pass
     results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
     try:
         print("{:<30}|  {:<}".format(i, results[0]))
